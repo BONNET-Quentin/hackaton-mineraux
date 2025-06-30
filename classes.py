bc41d3064009b836e0e481d2d13405783c9cf3a2
Noé Terrien <noeterrien@etu.minesparis.psl.eu>
2025-06-30 11:27:09 +0200

    test de l'initialisation

--- classes.py
+++ classes.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-# Fichier pour créer les classe
+# Fichier pour créer les classes
 
 class voxel :
     def __init__(self,coord,etat,voisins):
@@ -10,29 +10,29 @@ class voxel :
     
 def init_mat (h,L,n):
     
-    T=np.zeros((h,L))
+    T=np.zeros((h,L), dtype=object) 
     
     for i in range (h):      ##on définit la matrice 
             for j in range (L):
-                v=voxel((i,j),0)
+                v=voxel((i,j),0,[])
                 T[i,j]=v
             
             
-    s=int(L/2)-int(L/2)
-    f=int(L/2)+int(L/2)
+    s=L//2-n//2
+    f=h//2-n//2
     
-    for i in range (s,f):     ##on définit le cristal
-        for j in range (s,f):
+    for i in range (s,s+n):     ##on définit le cristal
+        for j in range (f,f+n):
             T[i,j].etat=1
     
     for i in range (h) :
         for j in range (L) :
             l=[]
-            if i+1<=h:
+            if i+1<h:
                 l.append(T[i+1,j])
             if j-1>=0:
                 l.append(T[i-1,j])
-            if j+1<=L:
+            if j+1<L:
                 l.append(T[i,j+1])
             if i-1>0: 
                 l.append(T[i-1,j])
