7a9818b44a40e249808c088a459b1608fcd0432a
Noé Terrien <noeterrien@etu.minesparis.psl.eu>
2025-06-30 10:53:35 +0200

    Ajout de l'interface graphique avec animation

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-# Fichier principal pour l'algo
+# Imports
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+# constantes
+h, L = 50,50
+
+# initialisation de la figure
+fig,ax = plt.subplots()
+
+# génération d'une liste de tableaux de booléens de taille (h, L) à afficher
+matrix = np.zeros((h,L), dtype=bool)
+ims = []
+for i in range(60):
+    matrix = (1-matrix)
+    im = ax.imshow(matrix, animated=True, cmap='gray', vmin=0, vmax=1)
+    if i == 0:
+        ax.imshow(matrix, cmap='gray', vmin=0, vmax=1)  # show an initial one first
+    ims.append([im])
+
+# animation
+ani = animation.ArtistAnimation(fig, ims, interval=1000, blit=True,
+                                repeat_delay=1000)
+
+# affichage
+plt.show()
