864c31fecf8b50e82bfd06405a05285cdca4e832
Arthur Lauret <arthur.lauret@orange.fr>
2025-06-30 16:06:31 +0200

    commitarthur3

--- voxel.py
+++ voxel.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random as rd 
 
 # Fichier pour créer les classes
 
@@ -7,10 +8,15 @@ class voxel :
         self.coord=coord
         self.etat=etat
         self.voisins=voisins
+
+@np.vectorize 
+def get_etat(v:voxel):
+    return v.etat
+    
     
 def init_mat (h,L,n):
     
-    T=np.zeros((h,L))
+    T=np.zeros((h,L), dtype=object)
     
     for i in range (h):      ##on définit la matrice 
             for j in range (L):
@@ -18,8 +24,8 @@ def init_mat (h,L,n):
                 T[i,j]=v
             
             
-    s=int(L/2)-int(L/2)
-    f=int(L/2)+int(L/2)
+    s=int(h/2)-int(n/2)
+    f=int(L/2)-int(n/2)
     
     for i in range (s,s+n):     ##on définit le cristal
         for j in range (f,f+n):
@@ -45,11 +51,29 @@ def conv (h,L,T):  ##fait un tableau affichable à partir du tableau de voxel
     A=np.zeros((h,L))
     for i in range (h):
         for j in range (L):
-            A[i,j]=T[i,j].etat
+            A[i,j]=get_etat(T[i,j])
     return A 
-            
-        
+
+def free_voxels (T):
+    M=T[get_etat(T)==1]   
+    L=[]
+    for m in M : 
+        for v in m.voisins :
+            if v.etat==0 :
+                L.append(v)
+    return L
+
+def update_mat (T):
+    l=free_voxels(T)
+    v=rd.choice(l)
+    v.etat=1
+    coord = v.coord[0], v.coord[1]
+    T[coord[0],coord[1]]=v
+    return T
     
-        
+
+    
+    
+
 
     
