c5c41974563a40e8be5a5885419167a52e7f2670
Noé Terrien <noeterrien@etu.minesparis.psl.eu>
2025-06-30 11:27:09 +0200

    test de l'initialisation

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from animation import generate_animation, example_matrices
+from classes import init_mat, conv
 
 # constantes
 h, L = 50,50
+n = 5 # taille du cristal initial
 
-generate_animation(example_matrices, interval=500)
+T = init_mat(h, L, n)
+A = conv(h, L, T)
+
+generate_animation([A], interval=500)
