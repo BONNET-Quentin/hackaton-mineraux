92016fa8188fca68c9ace80bd9299d62d22ad93b
Noé Terrien <noeterrien@etu.minesparis.psl.eu>
2025-07-01 11:15:58 +0200

    pour pull

--- main.py
+++ main.py
@@ -2,7 +2,10 @@
 # Imports
 from animation import generate_animation, example_matrices
 import animation3D as a3d
-from voxel import init_mat, conv, update_mat
+from voxel import init_mat, conv, update_mat, free_voxels, get_etat
+import time
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 # constantes
@@ -15,14 +18,27 @@ Nc = 100 # nombre de cristaux à générer
 # initialisation de la matrice de fluide avec un cristal au milieu
 T = init_mat(h, w, d, n)
 
-# Fonction de mise à jour de la matrice de voxels
-def update(i):
+# Fonction de mise à jour de la matrice de voxels()
+count = 5000
+start = time.time()
+for _ in range(count):
+    get_etat(T)
+end = time.time()
+print("temps moyen pour extraire les états : ", (end-start)/count)
+
+
+
+""" def update(i):
+    start = time.time()
     for k in range(10*(i**3)):
         update_mat(T)
-    return conv(T)
+    sim_num.append(10*(i**3))
+    durations.append(time.time()-start)
+    return conv(T) """
 
 # Génération de l'animation
-a3d.generate_animation(conv(T), lambda m,i : update(i), interval=500)
+# a3d.generate_animation(conv(T), lambda m,i : update(i), interval=500)
 
 # Exemple d'animation 2D
 # a3d.generate_animation(a3d.example_matrice, a3d.example_update, interval=500)
+
