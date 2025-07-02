41946970c7b183315436e4d36f7ff82964ab5979
Quentin Bonnet <quentin.bonnet@etu.minesparis.psl.eu>
2025-07-02 11:46:20 +0200

    version 6 otpimisée avec gif et couleurs

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 # Imports
 from animation import generate_animation, example_matrices
 import animation3D as a3d
-from voxel_sans_classe import init_mat, update_mat
+from voxel_sans_classe import init_mat, update_mat, cristal, bille
+import random as rd
 import time
+import numpy as np
 
 import matplotlib.pyplot as plt
 
 # constantes
-h, w, d = 30,30,30
+h, w, d = 50, 50, 50
 n = 1 # taille du cristal initial
 Nc = 5000 # nombre de cristaux à générer
 
 # initialisation de la matrice de fluide avec un cristal au milieu
-T = init_mat(h, w, d, n)
+T  = init_mat(h,w,d, 1)
+C = np.zeros((h,w,d), dtype = bool)
+for k in range (2):
+    T = cristal(T, rd.randint(5,10), rd.randint(5,10), rd.randint(5,10))
+    T = bille(T,rd.randint(2,6))
 """
 start = time.time()
 for _ in range(Nc):
@@ -23,9 +29,9 @@ print(f"Temps moyen pour la mise à jour : {(end - start)/Nc} secondes") """
 # Fonction de mise à jour de la matrice de voxels
 def update(i):
     for _ in range(10*(i**2)):
-        update_mat(T)
+        update_mat(T, C)
     print(f"Frame {i} générée")
-    return T
+    return T, C
 
 # Fonction de mise à jour de la matrice de voxels avec arrêt après un certain nombre de frames
 max_frames = 200
@@ -36,7 +42,7 @@ def update_with_stop(_, i):
     return update(i)
 
 # Génération de l'animation
-fig, ani = a3d.generate_animation(T, lambda m,i : update(i), interval=100, return_fig=True, show=False)
+fig, ani = a3d.generate_animation(T, C, lambda m,i : update(i), interval=100, return_fig=True, show=False)
 
 # Exemple d'animation 2D
 # a3d.generate_animation(a3d.example_matrice, a3d.example_update, interval=500)
