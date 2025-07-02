c5e6d2d0c0155d3aae414f15a0b8ef1c63a2fe68
Noé Terrien <noeterrien@etu.minesparis.psl.eu>
2025-07-02 10:42:33 +0200

    nouveau départ

--- main.py
+++ main.py
@@ -5,22 +5,21 @@ from voxel_sans_classe import init_mat, update_mat
 import time
 
 import matplotlib.pyplot as plt
-from matplotlib import animation
 
 # constantes
 h, w, d = 30,30,30
 n = 1 # taille du cristal initial
 Nc = 5000 # nombre de cristaux à générer
 
-# initialisation de la matrice de fluide avec un cristal au milieu
+""" # initialisation de la matrice de fluide avec un cristal au milieu
 T = init_mat(h, w, d, n)
 start = time.time()
 for _ in range(Nc):
     update_mat(T)
 end = time.time()
-print(f"Temps moyen pour la mise à jour : {(end - start)/Nc} secondes")
+print(f"Temps moyen pour la mise à jour : {(end - start)/Nc} secondes") """
 
-""" # Fonction de mise à jour de la matrice de voxels
+# Fonction de mise à jour de la matrice de voxels
 def update(i):
     for _ in range(10):
         update_mat(T)
@@ -35,7 +34,7 @@ def update_with_stop(_, i):
     return update(i)
 
 # Génération de l'animation
-a3d.generate_animation(T, lambda m,i : update(i), interval=500) """
+a3d.generate_animation(T, lambda m,i : update(i), interval=500)
 
 # Exemple d'animation 2D
 # a3d.generate_animation(a3d.example_matrice, a3d.example_update, interval=500)
