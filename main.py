21913cf0b7cb581c2a23bcedcb14a0544f1af299
Noé Terrien <noeterrien@etu.minesparis.psl.eu>
2025-07-02 10:24:00 +0200

    version voisins obtenus avec décalage propre

--- main.py
+++ main.py
@@ -14,13 +14,13 @@ Nc = 5000 # nombre de cristaux à générer
 
 # initialisation de la matrice de fluide avec un cristal au milieu
 T = init_mat(h, w, d, n)
-""" start = time.time()
+start = time.time()
 for _ in range(Nc):
     update_mat(T)
 end = time.time()
-print(f"Temps moyen pour la mise à jour : {(end - start)/Nc} secondes") """
+print(f"Temps moyen pour la mise à jour : {(end - start)/Nc} secondes")
 
-# Fonction de mise à jour de la matrice de voxels
+""" # Fonction de mise à jour de la matrice de voxels
 def update(i):
     for _ in range(10):
         update_mat(T)
@@ -35,7 +35,7 @@ def update_with_stop(_, i):
     return update(i)
 
 # Génération de l'animation
-a3d.generate_animation(T, lambda m,i : update(i), interval=500)
+a3d.generate_animation(T, lambda m,i : update(i), interval=500) """
 
 # Exemple d'animation 2D
 # a3d.generate_animation(a3d.example_matrice, a3d.example_update, interval=500)
